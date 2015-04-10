4229bb0b111bc9f70ddd0c5d166ee171c8865f10
colin.eatherton <colinmjeatherton@gmail.com>
2015-04-10 18:25:41 +0100

    created django project

--- vagrant/pg_config.sh
+++
@@ -1,6 +0,0 @@
-apt-get -qqy update
-apt-get -qqy install python-pip
-sudo pip install Django
-
-vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
-echo -e $vagrantTip > /etc/motd
